e768a88b6789f9c7c92affe3a83bd44e9dd74357
dongdong <jackzhangddong@163.com>
2024-10-01 11:32:11 +0800

    log组件err

--- MiaoCoin/src/util/log.js
+++ MiaoCoin/src/util/log.js
@@ -11,8 +11,14 @@ class Logger {
     log (data) {
         const now = new Date();
         const format_now = format(now, 'yyyy-MM-dd HH:mm:ss');
-        console.log(`${colors.green(`[${this.module_name}]`)}${colors.blue(`[${this.file_name}]`)}${colors.yellow(`[${format_now}]`)}${data}`);
+        console.log(`[LOG]${colors.green(`[${this.module_name}]`)}${colors.blue(`[${this.file_name}]`)}${colors.yellow(`[${format_now}]`)}${data}`);
+    }
+    err (data) {
+        const now = new Date();
+        const format_now = format(now, 'yyyy-MM-dd HH:mm:ss');
+        console.log(`${colors.bgRed('[ERR]')}${colors.red(`[${this.module_name}]`)}${colors.red(`[${this.file_name}]`)}${colors.red(`[${format_now}]`)}${colors.redBright(data)}`);
     }
 }
 module.exports = Logger
 
+new Logger(__filename).err('hell0')
